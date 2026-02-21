378006126d122fbd14ade022ab14c5c6e76923d7
Jesus Arambula <61527697+JesusEArambula@users.noreply.github.com>
2026-02-21 09:29:23 -0800

    Update file_management.py
    Messed around a little bit more with creating files and dictionary...

--- file_management.py
+++ file_management.py
@@ -9,6 +9,15 @@ print(os.listdir())
 # copyfile() = copies contents of a file
 # copy()     = copyfile() + permission mode + destination can be a directory
 # copy2()    = copy() + copies metadata (file's creation and modification times)
-shutil.copyfile('agreement form.docx', '../(#20 SPANISH) Mon/Cohort #20 Agreement Form.docx')
 
-print(os.listdir())
+# shutil.copyfile('agreement form.docx', '../(#20 SPANISH) Mon Wed March 2026 Workshop 360 Valencia/Cohort #20 Agreement Form.docx')
+# print(os.listdir())
+
+number = int(input("Enter number: "))
+my_dict = {}
+
+for i in range(number):
+    value = input(f"Enter item {i + 1} value: ")
+    my_dict[i] = "Cohort #" + value
+
+print(my_dict)
