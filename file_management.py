232c60423dff65a6883f5971b2a5502a31e82f59
Jesus Arambula <61527697+JesusEArambula@users.noreply.github.com>
2026-02-22 00:18:11 -0800

    Update file_management.py file
    Changed Cohort class variables again
    Included prompt to select days sections
    - 1) for mondays/wednesdays
    - 2) for tuesdays/thursdays

--- file_management.py
+++ file_management.py
@@ -14,22 +14,35 @@ print(os.listdir())
 # print(os.listdir())
 
 class Cohort:
-    def __init__ (self, cohort_id, cohort_num, cohort_month, cohort_day1, cohort_day2, cohort_year):
+    def __init__ (self, cohort_id, cohort_num, cohort_month, cohort_year, cohort_days, cohort_start, cohort_end):
         self.cohort_id = cohort_id
         self.cohort_num = cohort_num
         self.cohort_month = cohort_month
-        self.cohort_day1 = cohort_day1
-        self.cohort_day2 = cohort_day2
         self.cohort_year = cohort_year
+        self.cohort_days = cohort_days
+        self.cohort_start = cohort_start
+        self.cohort_end = cohort_end
+    
+
 
 number = int(input("Enter number: "))
-
 cohort_list = []
 
-
 for i in range(number):
+    print(f"<<<<<<<<<<<<<< Enter information for cohort {i + 1} >>>>>>>>>>>>>>")
+    num = input(f"Enter cohort number: ")
+    month = input(f"Enter cohort month: ")
+    year = input(f"Enter cohort year: ")
+    print("Select cohort's days.")
+    print("1) Monday/Wednesday")
+    print("2) Tuesday/Thursaday")
+    days = input(f"Enter cohort's days option (1/2): ")
+    start = input(f"Enter cohort start date: ")
+    end = input(f"Enter cohort end date: ")
+    
+    cohort = Cohort(i, num, month, year, days, start, end)
+    cohort_list.append(cohort)
 
-    cohort_num = input(f"Enter item {i + 1} value: ")
     
 
 print(cohort_list)
