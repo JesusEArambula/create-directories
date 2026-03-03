a52a759a4775375d8ed072e6f23c7a0d54d2dff3
Jesus Arambula <elielarambula@gmail.com>
2026-03-03 12:54:34 -0800

    Update window.py
    Added a move function to move objects in a canvas in the window
    And added /src folder to store all window code

--- src/window.py
+++ src/window.py
@@ -10,7 +10,7 @@ root.title("My First Application")
 root.configure(bg="lightblue")
 # Window Icon
 # PNG and GIF are reliably supported across platforms
-icon_image = tk.PhotoImage(file='res/folder-icon.png')
+icon_image = tk.PhotoImage(file='../res/folder-icon.png')
 root.iconphoto(True, icon_image)
 
 
@@ -31,6 +31,27 @@ root.minsize(int(screen_width * 0.5), int(screen_height * 0.5))
 center_x = int(screen_width/2 - window_width/2)
 center_y = int(screen_height/2 - window_height/2)
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+# TESTING AREA
+def move_object(event):
+    if event.keysym == 'Left':
+        canvas.move(my_circle, -10, 0) # Move left by 10 pixels
+    if event.keysym == 'Right':
+        canvas.move(my_circle, 10, 0) # Move right by 10 pixels
+    if event.keysym == 'Up':
+        canvas.move(my_circle, 0, -10) # Move Up by 10 pixels 
+    if event.keysym == 'Down':
+        canvas.move(my_circle, 0, 10) # Move Up by 10 pixels   
+
+canvas = tk.Canvas(root, width=300, height=300, bg="lightblue", highlightthickness=0)
+canvas.pack()
+
+my_circle = canvas.create_oval(10, 10, 50, 50, fill="red")
+
+# Bind keyboard events to the move_object function
+root.bind("<Key>", move_object)
+
+
 
 # 4. Start the application's main event loop
 # This keeps the window open and responsive to user interactions
